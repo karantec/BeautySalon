f47b32b704332ac71598dab0a1cc6c772a2b21f9
karananu <karanrana3095@gmail.com>
2023-05-18 21:12:56 +0530

    firstcommit

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -8,15 +8,15 @@ const Nav = () => {
             <div className="flex justify-between items-center py-6">
               <div>
                 
-                  <span className="text-xl font-bold text-gray-800">Event Booking</span>
+                  <span className="text-xl font-bold text-gray-800">DD Planner</span>
                
               </div>
               <div>
                 
-                  <span className="text-gray-500 hover:text-gray-800 mr-4">Log in</span>
+                  <span className="text-gray-500 hover:text-gray-800 mr-4">Home</span>
           
                
-                  <span className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">Sign up</span>
+                  <span className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">About</span>
               </div>
             </div>
           </div>
