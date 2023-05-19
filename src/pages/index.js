da3ba1cadf76adcf6cc520afc98507a21e103e8d
KARAN RANA <103360966+karananu@users.noreply.github.com>
2023-05-19 12:23:14 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,9 +33,9 @@ const index = () => {
       {/*<FeaturedEvent />*/}
       {/* <BrowseEvents /> */}
       <Contact />
-        <script  type="text/javascript">  var config = {    phone :"9798316854",    call :"Message Us", 
-                                                        position :"ww-right",    size : "ww-normal",    text : "",    type: "ww-standard",    brand: "",    subtitle: "",    welcome: ""  };  var proto = document.location.protocol, host = "cloudfront.net", url = proto + "//d3kzab8jj16n2f." + host;    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true; s.src = url + "/v2/main.js";    s.onload = function () { tmWidgetInit(config) };    
-var x = document.getElementsByTagName("script")[0]; x.parentNode.insertBefore(s, x);</script>0
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.2811099104774!2d86.14943017528877!3d23.66590307872815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4234692532021%3A0x526797e09e88bd48!2sCity%20centre%20sector%204!5e0!3m2!1sen!2sin!4v1684479098847!5m2!1sen!2sin"
+    width="600" height="450" style="border:0;" allowfullscreen=""
+    loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       <Footer />
       {/* <BookingPage /> */}
     </>);
