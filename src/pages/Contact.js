8d91bdd902d7cdb0f2726d6618e341cacd3113a7
KARAN RANA <103360966+karananu@users.noreply.github.com>
2023-05-19 12:26:12 +0530

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -44,7 +44,11 @@ export default function EventBooking() {
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md" type="submit" value="Send" />
                    
                 </form>
-                
+                <div>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.2811099104774!2d86.14943017528877!3d23.66590307872815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4234692532021%3A0x526797e09e88bd48!2sCity%20centre%20sector%204!5e0!3m2!1sen!2sin!4v1684479098847!5m2!1sen!2sin"
+    width="600" height="450" style="border:0;" allowfullscreen=""
+    loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
             </div>
         </>
     );
